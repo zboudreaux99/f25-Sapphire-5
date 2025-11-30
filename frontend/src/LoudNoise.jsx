cedbd7f9a5e68a677ae76463d231d1f0e99a556a
asman0011 <asman001@odu.edu>
2025-11-30 15:46:51 -0800

    Created LoudNoise button for trigger and added to App.jsx

---
+++ frontend/src/LoudNoise.jsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function LoudNoise(){
+    return (
+        <>
+            <Button type="button" className='mb-2 mt-2'> Button for LoudNoise test! </Button>
+        </>
+    )
+}
+
+export default LoudNoise
